2f11a0b13435f473b170ce118b7934a9040ce7de
R0mansky <romansky.wozniak@gmail.com>
2022-06-22 19:02:17 +0200

    Update new_appointment.dart

--- lib/pages/Appointments/new_appointment.dart
+++ lib/pages/Appointments/new_appointment.dart
@@ -15,21 +15,7 @@ class _NewAppointmentState extends State<NewAppointment> {
         centerTitle: true,
         title: const Text("Prendre rendez-vous"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        tooltip: '',
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
